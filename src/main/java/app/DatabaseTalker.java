7eb25dfaca6cc7c7ed1918d44174748962d525e2
Sondre Kolberg <sondrkol@stud.ntnu.no>
2022-09-19 21:02:50 +0200

    Skeleton

---
+++ src/main/java/app/DatabaseTalker.java
@@ -0,0 +1,61 @@
+package app;
+
+import java.util.Random;
+
+public class DatabaseTalker {
+    
+
+    private byte[] testHashFunction(byte[] key){//todo (create seperate class for hashing)
+        byte[] result = {1, 2, 3, 4};
+        return result;
+    }
+
+
+    private byte[] hashWithSalt(byte[] key, int salt){
+        byte[] keyPlusSalt = new byte[key.length+4];
+        for(int i = 0; i < key.length; i++){
+            keyPlusSalt[i] = key[i];
+        }
+
+        keyPlusSalt[key.length+0] = (byte)(salt & 0xff000000);
+        keyPlusSalt[key.length+1] = (byte)(salt & 0x00ff0000);
+        keyPlusSalt[key.length+2] = (byte)(salt & 0x0000ff00);
+        keyPlusSalt[key.length+3] = (byte)(salt & 0x000000ff);
+        return testHashFunction(keyPlusSalt);
+    }
+
+    private int getSalt(String username){//todo
+        return 0;
+    }
+
+    private byte[] getHashedPassPlusSalt(String username){//todo
+        byte[] result = {1, 10, 100, (byte)255};
+        return result;
+    }
+
+    private boolean userExists(String username){//todo
+        return false;
+    }
+
+    public void storeUser(String username, String password){
+        Random rand = new Random();
+        int salt = rand.nextInt();
+        byte[] passwordBytes = password.getBytes();
+        byte[] hashedPassPlusSalt = hashWithSalt(passwordBytes, salt);
+
+        //todo
+        //store(username, salt, hashedPassPlusSalt)
+    }
+
+
+    public boolean checkPassword(String username, String password){
+        if(userExists(username)){
+            int salt = getSalt(username);
+            byte[] hashedPassPlusSalt = getHashedPassPlusSalt(username);
+            if(hashedPassPlusSalt == hashWithSalt(password.getBytes(), salt)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
