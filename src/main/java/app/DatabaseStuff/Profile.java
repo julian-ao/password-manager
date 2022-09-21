3eb289f5ac0a2b776c43e8984d9f6bb139833b4d
Sondre Kolberg <sondrkol@stud.ntnu.no>
2022-09-21 16:53:08 +0200

    user/profile classes

---
+++ src/main/java/app/DatabaseStuff/Profile.java
@@ -0,0 +1,10 @@
+package app.DatabaseStuff;
+
+public class Profile {
+    private String url;
+    private String email;
+    private String profileUsername;
+    private String password;
+    private String profileNickname;
+
+}
---
+++ src/main/java/app/DatabaseStuff/User.java
@@ -0,0 +1,7 @@
+package app.DatabaseStuff;
+
+public class User {
+    private String username;
+    private int salt;
+    private byte[] hashedPassword;
+}
