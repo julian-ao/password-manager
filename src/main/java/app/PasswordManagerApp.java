ac83b1dcaf2df169377727ff3641ebfe8f31bced
Julian <julianao@stud.ntnu.no>
2022-09-21 19:53:10 +0200

    la til main pane i fxml fila (eksempel)

--- src/main/java/app/PasswordManagerApp.java
+++ src/main/java/app/PasswordManagerApp.java
@@ -12,7 +12,7 @@ public class PasswordManagerApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PasswordManagerApp.class.getResource("passwordManager.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
