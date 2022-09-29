6faf55518e8bf0e411c82f53e665796f0d1ceda3
Sondre Kolberg <sondrkol@stud.ntnu.no>
2022-09-29 20:41:37 +0200

    UserSession is now singleton pattern
    loginPage and passwordPage now uses UserSession.getInstance() to interact with the same object and share data
    
    Author @sondrkol

--- passwordManager/src/main/java/app/PasswordPageController.java
+++ passwordManager/src/main/java/app/PasswordPageController.java
@@ -1,11 +1,16 @@
 package app;
 
+import app.database.*;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class PasswordPageController extends PasswordManagerController {
@@ -21,8 +26,45 @@ public class PasswordPageController extends PasswordManagerController {
 
     @FXML
     public void initialize(){
+        userSession = UserSession.getInstance();
+
+        
+        ArrayList<GridPane> passwords = new ArrayList<GridPane>();
+        for(int i = 0; i < 5; i++){
+            passwords.add(passwordComponent(userSession.getUsername(), "••••••••••••", "kennbr@gmail.com"));
+        }
+
+        for (GridPane i : passwords) {
+            passwordList.getChildren().add(i);
+        }
+                 
+
     }
-    // PASSWORDS PAGE METHODS
+
+    private GridPane passwordComponent(String username, String password, String email) {
+        GridPane gridPane = new GridPane();
+        gridPane.setPrefHeight(90);
+        gridPane.setPrefWidth(580);
+
+        Text usernameText = new Text(username);
+        usernameText.setStyle("-fx-font: 40 system;");
+        usernameText.setWrappingWidth(290);
+
+        Text passwordText = new Text(password);
+        passwordText.setStyle("-fx-font: 40 system;-fx-text-alignment: right;");
+        passwordText.setWrappingWidth(290);
+
+        Text emailText = new Text(email);
+        emailText.setStyle("-fx-font: 25 system;");
+
+        gridPane.add(usernameText, 0, 0);
+        gridPane.add(passwordText, 1, 1);
+        gridPane.add(emailText, 0, 1);
+
+        return gridPane;
+    }
+
+    
 
     @FXML
     private void onLogOutButtonClick(ActionEvent event) throws IOException {
