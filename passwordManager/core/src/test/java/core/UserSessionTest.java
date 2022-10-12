fc6aaa4b9e6b740be4448769175f88b4a7a77ed0
Sondre Kolberg <sondrkol@stud.ntnu.no>
2022-10-12 17:10:48 +0200

    Tests for UserSession
    added tests to test the functionality of the UserSession class: insertProfile(), login(), registerUser() and userValidator()
    
    Author @sondrkol

--- passwordManager/core/src/test/java/core/UserSessionTest.java
+++ passwordManager/core/src/test/java/core/UserSessionTest.java
@@ -46,4 +46,51 @@ public class UserSessionTest {
     System.out.println();
 
   }
+
+  @Test
+  public void loginTest() {
+    User user1 = new User("User1", "password1");
+    User user2 = new User("User2", "password2");
+
+    userSession.getDatabaseTalker().insertUser(user1);
+    assertEquals(true, userSession.login(user1.getUsername(), user1.getPassword()));
+    userSession.logOut();
+    assertEquals(false, userSession.login(user1.getUsername(), "password2"));
+    assertEquals(false, userSession.login("not a user", "password"));
+    assertEquals(false, userSession.login(user2.getUsername(), user2.getPassword()));
+    userSession.getDatabaseTalker().insertUser(user2);
+    assertEquals(true, userSession.login(user2.getUsername(), user2.getPassword()));
+
+  }
+
+  @Test
+  public void registerUserTest() {
+    assertEquals(false, userSession.login("user1", "password1"));
+    userSession.registerUser("user1", "password1");
+    assertEquals(true, userSession.login("user1", "password1"));
+
+  }
+
+  @Test
+  public void userValidatorTest() {
+    userSession.registerUser("user1", "Password1");
+    assertEquals(
+        "Passwords do not match",
+        userSession.userValidator("someuser", "Password1!", "Password1!a"));
+    assertEquals(
+        "Username already taken",
+        userSession.userValidator("user1", "Password1!", "Password1!"));
+    assertEquals(
+        "Username must be between 3 and 30 characters long and contain only letters and numbers",
+        userSession.userValidator("us", "Password1!", "Password1!"));
+    assertEquals(
+        "Username must be between 3 and 30 characters long and contain only letters and numbers",
+        userSession.userValidator("us", "Password1!", "Password"));
+    assertEquals(
+        "Password must be between 6 and 30 characters long and contain at least one lowercase letter, one uppercase letter, one number and one special character",
+        userSession.userValidator("user123", "Pa", "Pa"));
+    assertEquals(
+        "OK",
+        userSession.userValidator("someuser", "Password1!", "Password1!"));
+  }
 }
