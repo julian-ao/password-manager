2d96812d2e72dfe6d81e06c320c5eb22477b858c
Sondre Kolberg <sondrkol@stud.ntnu.no>
2022-10-26 12:08:22 +0200

    Moving functionality to client module
    Moved userbuilder to client and renamed it to uservalidator, setup skeleton for methods in RestTalker, added the userValdidator method to ClientCore
    
    Author: @sondrkol
    
    relate: #54

---
+++ passwordManager/client/src/main/java/client/uservalidator/UserValidator.java
@@ -0,0 +1,76 @@
+package client.userbuilder;
+
+import core.database.DatabaseTalker;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+/**
+ * UserBuilder is a class that is used to build a user object.
+ */
+public class UserValidator {
+  private String username;
+  private String password;
+  private DatabaseTalker databaseTalker;
+  private Map<Predicate<String>, UsernameValidation> usernameValidation;
+  private Map<Predicate<String>, PasswordValidation> passwordValidation;
+
+  /**
+   * UserBuilder is a constructor for the UserBuilder class.
+   * 
+   * @param databaseTalker the databaseTalker used to check if the username is
+   *                       already taken
+   */
+  public UserBuilder(DatabaseTalker databaseTalker) {
+    this.databaseTalker = databaseTalker;
+
+    usernameValidation = new HashMap<Predicate<String>, UsernameValidation>();
+    usernameValidation.put((x) -> x.length() < 3, UsernameValidation.tooShort);
+    usernameValidation.put((x) -> x.length() > 30, UsernameValidation.tooLong);
+    usernameValidation.put((x) -> !Pattern.matches("[a-zA-Z0-9]{0,10000}", x),
+        UsernameValidation.invalidCharacters);
+    usernameValidation.put((x) -> databaseTalker.userExists(x),
+        UsernameValidation.alreadyTaken);
+
+    passwordValidation = new HashMap<Predicate<String>, PasswordValidation>();
+    passwordValidation.put((x) -> x.length() < 6, PasswordValidation.tooShort);
+    passwordValidation.put((x) -> x.length() > 30, PasswordValidation.tooLong);
+    passwordValidation.put((x) -> !(Pattern.matches(".{0,100}[a-z].{0,100}", x)
+        && Pattern.matches(".{0,100}[A-Z].{0,100}", x)
+        && Pattern.matches(".{0,100}[0-9].{0,100}", x)
+        && Pattern.matches(".{0,100}[!\"#$%&].{0,100}", x)),
+        PasswordValidation.diversityError);
+  }
+
+  /**
+   * checks is a username is a valid one.
+   * 
+   * @param username the username to be validated
+   * @return UsernameValidation enum which contains the response code
+   */
+  public UsernameValidation validateUsername(String username) {
+    for (Map.Entry<Predicate<String>, UsernameValidation> entry : usernameValidation.entrySet()) {
+      if (entry.getKey().test(username)) {
+        return entry.getValue();
+      }
+    }
+    return UsernameValidation.OK;
+  }
+
+  /**
+   * checks if a password is a valid one.
+   * 
+   * @param password the password to be validated
+   * @return PasswordValidation enum which contains the response code
+   */
+  public PasswordValidation validatePassword(String password) {
+    for (Map.Entry<Predicate<String>, PasswordValidation> entry : passwordValidation.entrySet()) {
+      if (entry.getKey().test(password)) {
+        return entry.getValue();
+      }
+    }
+    return PasswordValidation.OK;
+  }
+
+}
---
+++ passwordManager/client/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module name {
+  exports client;
+  exports client.uservalidator;
+}
