2408678db60d3218d0acd0ac03a92d760c46147a
Julian <julianao@stud.ntnu.no>
2022-11-08 12:28:16 +0100

    Added copy functionality
    Added a copy-to-clipboard button to each password component that copies the password as text to the users clipboard. Also addede a hover effect to it.
    
    Relates to #66
    
    Authors: @kennbr & @julianao

--- passwordManager/ui/src/main/java/ui/PasswordPageController.java
+++ passwordManager/ui/src/main/java/ui/PasswordPageController.java
@@ -2,23 +2,34 @@ package ui;
 
 import core.Password;
 import core.UserSession;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.GaussianBlur;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 import javafx.scene.text.Text;
 
 /**
@@ -61,6 +72,10 @@ public class PasswordPageController extends PasswordManagerController {
 
   private Password passwordObj;
 
+  final Clipboard clipboard = Clipboard.getSystemClipboard();
+
+  final ClipboardContent  clipboardContent = new ClipboardContent();
+
   /**
    * initialize sets the signed in as text to the username of the user
    * and updates the passwords based on the used session.
@@ -95,16 +110,15 @@ public class PasswordPageController extends PasswordManagerController {
    */
   private GridPane passwordComponent(String username, String title, String password) {
     GridPane gridPane = new GridPane();
+    gridPane.setPrefWidth(690);
     gridPane.setPadding(new Insets(15, 0, 20, 0));  
     gridPane.setBorder(new Border(new BorderStroke(Color.web("#A6A6A6"), 
             Color.web("#A6A6A6"), Color.web("#A6A6A6"), Color.web("#A6A6A6"),
-            BorderStrokeStyle.NONE, BorderStrokeStyle.NONE, BorderStrokeStyle.SOLID,
-            BorderStrokeStyle.NONE,
+            BorderStrokeStyle.NONE, BorderStrokeStyle.NONE, BorderStrokeStyle.SOLID, BorderStrokeStyle.NONE,
             CornerRadii.EMPTY, new BorderWidths(1), Insets.EMPTY)));
 
     Text titleText = new Text(title);
     titleText.setStyle("-fx-font: 40 system;");
-    titleText.setWrappingWidth(345);
 
     Text usernameText = new Text(username);
     usernameText.setStyle("-fx-font: 25 system;-fx-text-fill: red;");
@@ -113,11 +127,65 @@ public class PasswordPageController extends PasswordManagerController {
     Text passwordText = new Text(password);
     passwordText.setStyle("-fx-font: 25 system;-fx-text-alignment: right;");
     passwordText.setFill(Color.web("#A6A6A6"));
-    passwordText.setWrappingWidth(345);
+
+    // IMAGE
+    SVGPath copySVGPath = new SVGPath();
+    copySVGPath.setContent("M502.6 70.63l-61.25-61.25C435.4 3.371 427.2 0 418.7 0H255.1c-35.35 0-64 28.66-64 64l.0195 256C192 355.4 220.7 384 256 384h192c35.2 0 64-28.8 64-64V93.25C512 84.77 508.6 76.63 502.6 70.63zM464 320c0 8.836-7.164 16-16 16H255.1c-8.838 0-16-7.164-16-16L239.1 64.13c0-8.836 7.164-16 16-16h128L384 96c0 17.67 14.33 32 32 32h47.1V320zM272 448c0 8.836-7.164 16-16 16H63.1c-8.838 0-16-7.164-16-16L47.98 192.1c0-8.836 7.164-16 16-16H160V128H63.99c-35.35 0-64 28.65-64 64l.0098 256C.002 483.3 28.66 512 64 512h192c35.2 0 64-28.8 64-64v-32h-47.1L272 448z");
+
+    // COLOR
+    BackgroundFill greyBackgroundFill = new BackgroundFill(Color.valueOf("#A6A6A6"), new CornerRadii(10), new Insets(10));
+    Background greyBackground = new Background(greyBackgroundFill);
+
+    // TOGETHER
+    Region copyRegion = new Region();
+    copyRegion.setBackground(greyBackground);
+    copyRegion.setShape(copySVGPath);
+    copyRegion.setMinSize(50, 50);
+    copyRegion.setMaxSize(50, 50);
+
+    copyRegion.setOnMouseClicked(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        clipboardContent.putString(password);
+        clipboard.setContent(clipboardContent);
+        System.out.println("Copied to clipboard");
+      }
+    });
+
+    copyRegion.setOnMouseEntered(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        scene = copyRegion.getScene();
+        copyRegion.setBackground(new Background(new BackgroundFill(Color.valueOf("#D9D9D9"), new CornerRadii(10), new Insets(10))));
+        scene.setCursor(Cursor.HAND); //Change cursor to hand
+      }
+    });
+
+    copyRegion.setOnMouseExited(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        scene = copyRegion.getScene();
+        copyRegion.setBackground(new Background(new BackgroundFill(Color.valueOf("#A6A6A6"), new CornerRadii(10), new Insets(10))));
+        scene.setCursor(Cursor.DEFAULT); //Change cursor to default
+      }
+    });
+
+    clipboardContent.putString(password);
+    clipboard.setContent(clipboardContent);
+    
+    ColumnConstraints col1 = new ColumnConstraints();
+    col1.setPercentWidth(50);
+    ColumnConstraints col2 = new ColumnConstraints();
+    col2.setPercentWidth(40);
+    ColumnConstraints col3 = new ColumnConstraints();
+    col3.setPercentWidth(10);
+    
+    gridPane.getColumnConstraints().addAll(col1,col2,col3);
 
     gridPane.add(titleText, 0, 0);
     gridPane.add(usernameText, 0, 1);
     gridPane.add(passwordText, 1, 1);
+    gridPane.add(copyRegion, 2, 1);
 
     return gridPane;
   }
